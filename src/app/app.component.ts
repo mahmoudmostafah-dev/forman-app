c569f5a4cd458b3fadfff918afe2e627fd05342d
mahmoudmostafah-dev <aooda2010@hotmail.com>
2024-09-25 18:59:07 +0300

    "Added crypto-js and @types/crypto-js dependencies, updated i18n files, and modified app component and header component with new imports and functionality."

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './pages/header/header.component';
+import { ChangeDirService } from './shared/service/change-dir.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
+  providers: [ChangeDirService]
 })
 export class AppComponent {
   title = 'forman-app';
+  changeLangService = inject(ChangeDirService);
+  ngOnInit() {
+    this.changeLangService.setDefaultLang();
+  }
 }
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { SharedButtonComponent } from '../../shared/components/shared-button/shared-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  imports: [SharedButtonComponent, TranslateModule]
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  click($event: void) {}
+}
