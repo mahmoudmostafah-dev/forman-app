e3a2dec71b5592717b66e574a35ad1c2f674f7e1
Mohamed Anwar <mohamedanwar2000k@gmail.com>
2024-10-04 16:10:42 +0300

    Commit

---
+++ src/app/pages/guild-book/guild-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guild-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './guild-book.component.html',
+  styleUrl: './guild-book.component.scss'
+})
+export class GuildBookComponent {
+
+}
