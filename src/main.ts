b08922ea3f04fe8c26a4bd693f6b39a059bca365
Mohamed Anwar <mohamedanwar2000k@gmail.com>
2024-10-04 15:16:19 +0300

    First commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+/*
+  =========================
+  == ACCORDION IN FOOTER ==
+  =========================
+*/
+const accordionBtn1 = document.querySelectorAll<HTMLImageElement>(".quastion-header img:nth-of-type(1)");
+const accordionBtn2 = document.querySelectorAll<HTMLImageElement>(".quastion-header img:nth-of-type(2)");
+const accordionList = document.querySelectorAll<HTMLElement>(".quastion .content");
+
+for (let i = 0; i < accordionList.length; i++) {
+  accordionBtn1[i].onclick = () => {
+    accordionBtn1[i].style.display = "none";
+    accordionBtn2[i].style.display = "inline";
+    accordionList[i].classList.add("open");
+  };
+}
+
+for (let i = 0; i < accordionList.length; i++) {
+  accordionBtn2[i].onclick = () => {
+    accordionBtn2[i].style.display = "none";
+    accordionBtn1[i].style.display = "inline";
+    accordionList[i].classList.remove("open");
+  };
+}
