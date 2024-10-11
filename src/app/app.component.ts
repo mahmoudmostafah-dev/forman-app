74a04e5c9223fa0cc7f1e4ca9ec3231ed6676291
mahmoudmostafah-dev <aooda2010@hotmail.com>
2024-10-11 19:24:02 +0300

    "Updated import paths and formatting in app.component.ts and header.component.ts"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FaqsComponent } from "./pages/faqs/faqs.component";
-import { FooterComponent } from "./pages/footer/footer.component";
-import { GuildBookComponent } from "./pages/guild-book/guild-book.component";
-import { HeaderComponent } from './pages/header/header.component';
+import { HeaderComponent } from './pages/Components/header/header.component';
+import { FaqsComponent } from './pages/faqs/faqs.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { GuildBookComponent } from './pages/guild-book/guild-book.component';
 import { SocialComponent } from './pages/social/social.component';
-import { TransformationsComponent } from "./pages/transformations/transformations.component";
+import { TransformationsComponent } from './pages/transformations/transformations.component';
 import { CardComponent } from './shared/components/card/card.component';
 import { ChangeDirService } from './shared/service/change-dir.service';
 
@@ -15,7 +15,17 @@ import { ChangeDirService } from './shared/service/change-dir.service';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, HeaderComponent, CommonModule, CardComponent, GuildBookComponent, SocialComponent, FaqsComponent, TransformationsComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CommonModule,
+    CardComponent,
+    GuildBookComponent,
+    SocialComponent,
+    FaqsComponent,
+    TransformationsComponent,
+    FooterComponent
+  ],
   providers: [ChangeDirService]
 })
 export class AppComponent {
--- src/app/pages/Components/header/header.component.ts
+++ src/app/pages/Components/header/header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { SharedButtonComponent } from '../../shared/components/shared-button/shared-button.component';
+import { SharedButtonComponent } from '../../../shared/components/shared-button/shared-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   imports: [SharedButtonComponent, TranslateModule]
